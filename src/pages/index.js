a933800a224fcb46599d8500b805ddf46a2fc380
Loris <loris@maisondunet.com>
2019-07-10 09:12:58 +0200

    x

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,7 +52,7 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(limit: 1 sort: {fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           excerpt
